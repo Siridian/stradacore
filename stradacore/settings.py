8666f7b9be5d5f4d6c2f28ccfa081b51252d8de0
Victor <vsorba@vps741630.ovh.net>
2019-11-20 10:55:45 +0100

    cleared staticfiles folder from versioning + environment variables for allowed hosts

--- stradacore/settings.py
+++ stradacore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('SERVER_IP'), os.environ.get('DOMAIN_NAME')]
 
 
 # Application definition
