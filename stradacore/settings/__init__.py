e3ef12fae8c4090b485afeb4a8a3899ea3f9e555
The Great Siridian <victor.sorba@gmail.com>
2019-11-20 14:20:52 +0100

    settings module

--- stradacore/settings/__init__.py
+++ stradacore/settings/__init__.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('SERVER_IP'), os.environ.get('DOMAIN_NAME')]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
