dd8dbc7223ac0be00eb86d25b499c79613f8b13b
The Great Siridian <victor.sorba@gmail.com>
2019-11-26 13:14:38 +0100

    added travis files

---
+++ stradacore/settings/travis.py
@@ -0,0 +1,14 @@
+from . import *
+
+SECRET_KEY = 'blablablalblalblelgkr'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
