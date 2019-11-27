599acd9de399f5068a71c572db5d0b3356691612
The Great Siridian <victor.sorba@gmail.com>
2019-11-27 20:11:49 +0100

    Update admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -9,16 +9,25 @@ from django.contrib import admin
 from recipes.models import IngredientType, Ingredient, Recipe, RecipeIngredient
 
 admin.site.register(IngredientType)
-admin.site.register(Ingredient)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    radio_fields = {"type": admin.VERTICAL}
+
 
 
 class RecipeIngredientInline(admin.TabularInline):
+    autocomplete_fields = ['ingredient']
+    fields = ['ingredient', 'quantity', 'unit']
     model = RecipeIngredient
     extra = 1
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    fields = ('name', 'directions', 'type', 'pdf_file')
+    fields = ('name', 'directions', 'pdf_file')
+    radio_fields = {"type": admin.VERTICAL}
     inlines = (RecipeIngredientInline,)
 
